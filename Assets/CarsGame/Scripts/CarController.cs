c4f102d7adc3adc5535b267b87558ef440a5add5
Lautaro Coccia <lautaroauditore@gmail.com>
2021-11-02 09:26:03 -0300

    Add  car controller

---
+++ Assets/CarsGame/Scripts/CarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] float maxSpeed;
+    [SerializeField] float speed;
+    [SerializeField] Rigidbody rigidbody;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
